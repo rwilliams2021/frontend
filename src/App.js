b522b55f611bd075e9091d786ece2ec71ef30d9c
rwilliams <richyrichw93@gmail.com>
2024-05-30 10:51:57 +0200

    add carousel for topMeals

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Navbar } from './component/Navbar/Navbar';
 import { ThemeProvider } from '@emotion/react';
 import { darkTheme } from './theme/DarkTheme';
 import { CssBaseline } from '@mui/material';
+import { Home } from './component/Home/Home';
 
 function App() {
   return (
     <ThemeProvider theme = {darkTheme}>
       <CssBaseline />
       <Navbar/>
+      <Home/>
     </ThemeProvider>
   );
 }
---
+++ src/component/Home/TopMeals.js
@@ -0,0 +1,35 @@
+
+export const TopMeals=[
+    {
+        image:"https://cdn.pixabay.com/photo/2017/08/06/06/43/pizza-2589575_640.jpg",
+        title:"Pizza",
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_640.jpg",
+        title:"Burger",
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2017/10/15/11/41/sushi-2853382_640.jpg",
+        title:"Sushi",
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2023/05/29/17/01/hamburger-8026582_1280.jpg",
+        title:"Sandwich",
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2022/05/20/08/55/pasta-7209002_640.jpg",
+        title:"Salad",
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2014/01/24/04/05/fried-chicken-250863_640.jpg",
+        title:"Fried Chicken",
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2015/04/10/00/41/food-715542_640.jpg",
+        title:"Poke Bowl",
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2023/05/27/12/35/noodles-8021415_640.png",
+        title:"Noodles",
+    },
+]
