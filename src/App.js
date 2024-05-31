bf4084724e088784f7a5b65caf003942e5be6494
rwilliams <richyrichw93@gmail.com>
2024-05-31 12:32:55 +0200

    create Cart Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css';  
 import { Navbar } from './component/Navbar/Navbar';
 import { ThemeProvider } from '@emotion/react';
 import { darkTheme } from './theme/DarkTheme';
 import { CssBaseline } from '@mui/material';
 import { Home } from './component/Home/Home';
 import RestaurantDetails from './component/Restaurant/RestaurantDetails';
+import Cart from './component/Cart/Cart';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <CssBaseline />
       <Navbar/>
       {/* <Home/> */}
-      <RestaurantDetails/>
+      {/* <RestaurantDetails/> */}
+      <Cart/>
     </ThemeProvider>
   );
 }
